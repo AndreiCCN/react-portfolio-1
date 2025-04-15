ed452a936c1b2e3f62b114b65b7112b7e06a0180
Nicolae-Andrei Cuconoiu <andreicuconoiu@gmail.com>
2025-04-15 14:55:42 +0300

    feat: Added new icon & banner on home section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { ArrowDownIcon, DownloadIcon } from "./Icons";
+import { ArrowDownIcon, DownloadIcon, PersonIcon } from "./Icons";
 import Button from "./Button";
 
 const Hero = () => {
@@ -8,14 +8,8 @@ const Hero = () => {
       <div className="container items-center lg:grid lg:grid-cols-2 lg:gap-10">
         <div>
           <div className="flex items-center gap-3">
-            <figure className="img-box w-9 h-9 rounded-lg">
-              <Image
-                src=""
-                alt="Nicolae-Andrei Cuconoiu Portrait"
-                width={40}
-                height={40}
-                className="img-cover"
-              />
+            <figure className="img-box w-9 h-9 flex items-center justify-center rounded-lg">
+              <PersonIcon w={25} h={25} fillColor="#ffffff" />
             </figure>
             <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
               <span className="relative w-2 h-2 rounded-full bg-emerald-400">
@@ -47,13 +41,13 @@ const Hero = () => {
           </div>
         </div>
         <div className="hidden lg:block">
-          <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from=sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
+          <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
             <Image
-              src=""
+              src="/images/hero-banner.png"
               alt="Nicolae-Andrei Cuconoiu"
               width={656}
               height={800}
-              className=""
+              className="w-full"
             />
           </figure>
         </div>
